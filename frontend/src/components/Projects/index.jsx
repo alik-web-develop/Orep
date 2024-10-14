0e0dc84fda79906fd0223b94b80326ea5b23ccda
bulyon228 <alijongulyamov02@gmail.com>
2024-10-14 06:46:40 +0500

    update jsx files and small fix for style

--- frontend/src/components/Projects/index.jsx
+++ frontend/src/components/Projects/index.jsx
@@ -1,58 +1,37 @@
-import React from 'react'
-import { useState } from 'react'
-import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
-import ProjectCard from '../Cards/ProjectCard/ProjectCards.jsx'
-import { projects } from '../../data/constants'
+import React, { useState } from 'react';
+import './style.scss';
+import ProjectCard from '../Cards/ProjectCard/ProjectCards.jsx';
+import { projects } from '../../data/constants';
 
-
-const Projects = ({openModal,setOpenModal}) => {
+const Projects = ({ openModal, setOpenModal }) => {
   const [toggle, setToggle] = useState('all');
-  return (
-    <Container id="projects">
-      <Wrapper>
-        <Title>Projects</Title>
-        <Desc>
-          I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
-        </Desc>
-        <ToggleButtonGroup >
-          {toggle === 'all' ?
-            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
-            :
-            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
-          }
-          <Divider />
-          {toggle === 'web app' ?
-            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
-            :
-            <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
-          }
-          <Divider />
-          {toggle === 'android app' ?
-            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
-            :
-            <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
-          }
-          <Divider />
-          {toggle === 'machine learning' ?
-            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
-            :
-            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
-          }
-        </ToggleButtonGroup>
-        <CardContainer>
-          {toggle === 'all' && projects
-            .map((project) => (
-              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
-            ))}
-          {projects
-            .filter((item) => item.category == toggle)
-            .map((project) => (
-              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
-            ))}
-        </CardContainer>
-      </Wrapper>
-    </Container>
-  )
-}
 
-export default Projects
+  return (
+    <div className="projects-container" id="projects">
+      <div className="projects-wrapper">
+        <div className="projects-title">Projects</div>
+        <div className="projects-desc">
+          I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
+        </div>
+        <div className="projects-toggle-button-group">
+          {['all', 'web app', 'android app', 'machine learning'].map((category) => (
+            <div
+              key={category}
+              className={`projects-toggle-button ${toggle === category ? 'active' : ''}`}
+              onClick={() => setToggle(category)}
+            >
+              {category === 'web app' ? "WEB APP'S" : category.toUpperCase()}
+            </div>
+          ))}
+        </div>
+        <div className="projects-card-container">
+          {(toggle === 'all' ? projects : projects.filter((item) => item.category === toggle)).map((project) => (
+            <ProjectCard key={project.id} project={project} openModal={openModal} setOpenModal={setOpenModal} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
