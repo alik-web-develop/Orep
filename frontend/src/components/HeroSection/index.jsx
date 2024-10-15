8f687cb92afd96564eb5f12fec043bcd5301ea64
? <ilhomjonovasadbek123@gmail.com>
2024-10-15 20:32:05 +0500

    add db.json

--- frontend/src/components/HeroSection/index.jsx
+++ frontend/src/components/HeroSection/index.jsx
@@ -3,9 +3,12 @@ import HeroBgAnimation from '../HeroBgAnimation';
 import './HeroSection.scss'; // Импорт стилей
 import Logo from '../../images/logo.jpg';
 import Typewriter from 'typewriter-effect';
-import { Bio } from '../../data/constants';
+import Bio from "../../db.json"; 
 
 const HeroSection = () => {
+
+  const bioData = Bio.Bio[0];
+
   return (
     <div id="about" className="hero-container">
       <div className="hero-bg">
@@ -13,21 +16,21 @@ const HeroSection = () => {
       </div>
       <div className="hero-inner-container">
         <div className="hero-left-container">
-          <div className="hero-title">Hello, we are <br /> {Bio.name}</div>
+          <div className="hero-title">Hello, we are <br /> {bioData.name}</div>
           <div className="hero-text-loop">
             <span className="hero-span-name">We are </span>
             <span className="hero-span">
               <Typewriter
                 options={{
-                  strings: Bio.roles,
+                  strings: bioData.roles, 
                   autoStart: true,
                   loop: true,
                 }}
               />
             </span>
           </div>
-          <div className="hero-subtitle">{Bio.description}</div>
-          <a href="#" target='display' className="hero-resume-button">Check Resume</a>
+          <div className="hero-subtitle">{bioData.description}</div>
+          <a href="#" target='_blank' rel='noopener noreferrer' className="hero-resume-button">Check Resume</a> 
         </div>
         <div className="hero-right-container">
           <img src={Logo} alt="hero" className="hero-img" />
