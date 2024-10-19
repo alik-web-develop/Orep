39358f9fbe0c8465767af2196e9c2ba23faacc71
? <ilhomjonovasadbek123@gmail.com>
2024-10-20 01:56:10 +0500

    courses style

--- frontend/src/components/Courses/Item.jsx
+++ frontend/src/components/Courses/Item.jsx
@@ -1,10 +1,10 @@
-import { useContext, memo } from 'react'
+import React, { useContext, memo, useState } from 'react'
 import { context } from "../../store"
 import CoursesJSON from "../../db/Courses.json"
 
 function Item(props) {
     const { state, dispatch } = useContext(context)
-
+    const [showMore, setShowMore] = useState(false);
     // function addToWishlist(e) {
     //     e.stopPropagation()
     //     if (state.selectedProducts.includes(props.id)) {
@@ -24,17 +24,28 @@ function Item(props) {
     //         dispatch({ type: "ADD_TO_BASKET", payload: { id: props.id, count: 1, price: product.price } })
     //     }
     // }
+    const toggleShowMore = () => {
+        console.log("Toggle show more clicked");
+        setShowMore(!showMore);
+    };
+
+
 
     return (
         <div className="product-item">
             <div className="img-wrapper">
-                <img src={props.img} alt={props.title} onClick={(e) => e.target.requestFullscreen()} />
+                <img src={props.img} alt={props.title} />
             </div>
             <h4>{props.title}</h4>
-            <p className="desc">{props.desc}</p>
-            <p className="price">{props.price}</p>
+            <p className={`desc ${showMore ? 'show-full' : ''}`}>
+                {props.desc}
+            </p>
+            <span className="show-more" onClick={toggleShowMore}>
+                {showMore ? '...show less' : '...show more'}
+            </span>
+            <p className="price">${props.price}</p>
         </div>
     )
 }
 
-export default memo(Item)
+export default React.memo(Item)
