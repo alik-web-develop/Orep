89a2602c001f0a083aeb83698ccdb9f6f83868f0
bulyon228 <alijongulyamov02@gmail.com>
2024-11-09 09:16:04 +0500

    add to animation

--- frontend/src/components/AnimationComp/index.jsx
+++ frontend/src/components/AnimationComp/index.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useRef } from 'react';
-import './styles.scss'; // Подключаем стили
+import React, { useEffect, useRef, useState } from 'react';
+import './styles.scss';
 
 const AnimatedComponent = ({ children }) => {
   const ref = useRef(null);
+  const [hasAnimated, setHasAnimated] = useState(false); // Флаг для отслеживания, проигрывалась ли анимация
 
   useEffect(() => {
     const handleScroll = () => {
       const element = ref.current;
-      if (element) {
+      if (element && !hasAnimated) { // Проверка, если анимация еще не проигрывалась
         const rect = element.getBoundingClientRect();
         if (rect.top < window.innerHeight && rect.bottom > 0) {
           element.classList.add('active');
-        } else {
-          element.classList.remove('active');
+          setHasAnimated(true); // Устанавливаем флаг, чтобы анимация не повторялась
         }
       }
     };
@@ -23,7 +23,7 @@ const AnimatedComponent = ({ children }) => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [hasAnimated]);
 
   return (
     <div ref={ref} className="animation">
